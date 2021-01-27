aea5509bfbc8979c6bb4c0cc58b761b4ea41d056
root <root@adi>
2021-01-27 18:11:53 +0000

    fix: menu configuration passing arguments

--- backend/votacion.py
+++ backend/votacion.py
@@ -40,8 +40,8 @@ class Votation:
                 print('****** SISTEMA DE VOTACIONES ******')
                 print('\t 1. Crear votación \n',
                 '\t 2. Añadir candidato a una votación \n',
-                '\t 3. Cerrar lista de candidatos \n',
-                '\t 4. Votar candidato \n',
+                '\t 3. Votar candidato \n',
+                '\t 4. Cerrar lista de candidatos \n',
                 '\t 5. Consultar candidatos \n',
                 '\t 6. Consultar resultado de los candidatos \n',
                 '\t 7. Cerrar votación \n',
@@ -53,14 +53,44 @@ class Votation:
                     operaciones = {
                     '1': self.create_votation,
                     '2': self.add_candidate,
-                    '3': self.close_list,
-                    '4': self.vote,
+                    '3': self.vote,
+                    '4': self.close_list,
                     '5': self.list_candidates,
                     '6': self.view_winner,
                     '7': self.close_votation
                     }
-                    operaciones[str(opt)]()
+                    if (opt == 1):
+                        operaciones[str(opt)]()
 
+                    elif (opt > 3 and opt < 8):
+                         
+                        while True:
+                            try:
+                                votation_id = int(input("-> Introduce identificador de votación: "))
+                                args = [votation_id]
+                                break
+                                                        
+                            except ValueError:
+                                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
+                            
+                        operaciones[str(opt)](*args)
+                    
+                    elif (opt > 1 and opt < 4):
+                        
+                        while True:
+                            try:
+                            
+                                votation_id = int(input("-> Introduce identificador de votación: "))
+                                candidate = input("-> Introduce candidato: ")
+                                args = [votation_id, candidate]
+                                break
+                                
+
+                            except ValueError:
+                                print('¡¡¡Error, no se ha introducido algún valor correcto!!!\n')
+
+                        operaciones[str(opt)](*args)
+                
                 elif (opt == 8):
                     print('¡Buen día! \n')
                     sys.exit(0)
@@ -79,142 +109,102 @@ class Votation:
         transaction = self.contract.functions.crearVotacion().transact({'from': self.owner})  
         
         receipt = self.w3.eth.getTransactionReceipt(transaction)
-        print(receipt)
-
         block_id = receipt['blockNumber'] - 1
         
+        
         try:
-            votacion_id = self.contract.functions.crearVotacion().call(block_identifier=block_id)
+            votation_id = self.contract.functions.crearVotacion().call(block_identifier=block_id)
+        
+            print(f'Votación creada. (Id. votación: {votation_id})')
+            return votation_id
         
         except exceptions.Solidity as error:
-            print(error)
-            
-        
-        print(f'Votación creada. (Id. votación: {votacion_id})')
-        
+            print(error) 
 
-    def add_candidate(self):
+    def add_candidate(self, *args):
         '''
         Añadir un único candidato a un único id
         '''
-        votation_id = 0
-        candidate = ''
-                
-        while True:
-            try:
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                candidate = input("-> Introduce candidato: ")
-                break
-
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!!\n')
+        votation_id = args[0]
+        candidate = args[1]
 
         try:
             print('GAS estimation:', self.contract.functions.addCandidato(votation_id, candidate).estimateGas())
 
             transaction = self.contract.functions.addCandidato(votation_id, candidate).transact({'from': self.owner})
             receipt = self.w3.eth.getTransactionReceipt(transaction)
-            print(receipt)
+
             print(f'Candidato "{candidate}" añadido. (Id. votación: {votation_id}) \n')
             
-        except exceptions.SolidityError as error:
+            return receipt['status']    
+            
+        except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, el candidato está añadido \n')
-            print(error)
+            
 
-
-    def close_list(self):
+    def close_list(self, *args):
         '''
         Cerrar lista de candidatos
         '''
-        votation_id = 0
-
-        while True:
-            try:
-
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                break
-            
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
+        votation_id = args[0]
 
         try:
             print('GAS estimation:', self.contract.functions.cerrarLista(votation_id).estimateGas())
             transaction = self.contract.functions.cerrarLista(votation_id).transact({'from': self.owner})
             receipt = self.w3.eth.getTransactionReceipt(transaction)
-            print(receipt)
+
             print(f'Lista de candidatos cerrada. (Id. votación: {votation_id}) \n')
+            
+            return receipt['status']
 
         except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, la lista de candidatos no está cerrada \n')
 
 
-    def close_votation(self):
+    def close_votation(self, *args):
         '''
         Cerrar una única votación
         '''
-        while True:
-            try:
-
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                break
-
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
-
+        votation_id = args[0]
 
         try:
             print('GAS estimation:', self.contract.functions.cerrarEncuesta(votation_id).estimateGas())
             transaction = self.contract.functions.cerrarEncuesta(votation_id).transact({'from': self.owner})
             receipt = self.w3.eth.getTransactionReceipt(transaction)
-            print(receipt)
+
             print(f'Votación cerrada. (Id. votación: {votation_id}) \n')
+    
+            return receipt['status']
 
         except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, la votación no está cerrada \n')
 
 
-    def vote(self):
+    def vote(self, *args):
         '''
         Votar a un candidato
         '''
-        votation_id = 0
-        candidate = ''
-
-        while True:
-            try:
-
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                candidate = input("-> Introduce candidato: ")
-                break
-
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
+        votation_id = args[0]
+        candidate = args[1]
 
         try:
             print('GAS estimation:', self.contract.functions.votar(votation_id, candidate).estimateGas())
             transaction = self.contract.functions.votar(votation_id, candidate).transact({'from': self.owner})
             receipt = self.w3.eth.getTransactionReceipt(transaction)
-            print(receipt)
+            
             print(f'Candidato "{candidate}" votado. (Id. votación: {votation_id})')
+
+            return receipt['status']
 
         except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, la votación no se ha realizado correctamente. Indique de la ID de la votación y el candidato \n')
 
 
-    def list_candidates(self):
+    def list_candidates(self, *args):
         '''
         Listar nombres de los candidatos
         '''
-        votation_id = 0
-
-        while True:
-            try:
-
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                break
-
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
+        votation_id = args[0]
 
         print(f'Candidatos (Id. votación: {votation_id}): ')
         
@@ -223,27 +213,22 @@ class Votation:
             for candidate in candidates:
                 print(f'\t \t - {candidate} ')
         
+            return candidates
+
         except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, la lista de candidatos no está disponible \n')
 
-    def view_winner(self):
+    def view_winner(self,*args):
         '''
         Obtener ganador de una votación
         '''
-        votation_id = 0
-
-        while True:
-            try:
-
-                votation_id = int(input("-> Introduce identificador de votación: "))
-                break
-
-            except ValueError:
-                print('¡¡¡Error, no se ha introducido algún valor correcto!!! \n')
+        votation_id = args[0]
 
         try:
             winner = self.contract.functions.ganador(votation_id).call()
             print(f'Candidato "{winner}" ganador. (Id. votación: {votation_id})')
+        
+            return winner
         
         except exceptions.SolidityError:
             print('\n¡¡¡Error!!!, el candidato ganador no está disponible \n')
